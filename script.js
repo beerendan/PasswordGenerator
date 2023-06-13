c9e7596565c8c27d00265927b62e97a3e66123c8
Beerendan <brendanrbowen@gmail.com>
2023-06-13 14:11:37 -0400

    finally works

--- script.js
+++ script.js
@@ -60,6 +60,7 @@ function generatePassword() {
     var attributeArray= [];
     var confirmTotal=0;
 
+    //user selection of character type
     while(confirmCheck==false){
         var pLowercase= confirm ("Would you like to include lowercase characters?");
         var pUppercase= confirm ("Would you like to include uppercase characters?");
@@ -95,6 +96,73 @@ function generatePassword() {
 //Begin generating password with length and attributes selected
 console.log("Begin generating password with" +passwordLength +"characters");
 
+while(confirmCheck==true){
+    if (confirmTotal <1){
+        break;
+    }
+    
+    for (var z=0; z <passwordLength; z++){
+        var randomSelect= Math.floor(Math.random()*confirmTotal);
+        var attValue= attributeArray[randomSelect];
+
+        if(attValue== "L"){
+            //Uppercase array needs to be converted to lowercase
+            var holder=alphabetical().toLowerCase();
+            var temp=generatedPassword;
+            generatedPassword=temp.concat(holder);
+
+        } else if(attValue== "U"){
+            var holder= alphabetical();
+            var temp= generatedPassword;
+            generatedPassword=temp.concat(holder);
+            
+        } else if(attValue== "N"){
+            var holder= numeric();
+            var temp= generatedPassword;
+            generatedPassword= temp.concat(holder);
+
+        } else if (attValue== "S"){
+            var holder= special();
+            var temp= generatedPassword;
+            generatedPassword= temp.concat(holder);
+        }
+    }
+    confirmCheck=false;
+}
+
+//putting together generatedPassword based on attributes
+console.log("Attributes:" +
+"\n- Length:" +passwordLength +
+"\n- Lowercase" +pLowercase +
+"\n- UPPERCASE" +pUppercase +
+"\n- Numeric" +pNumeric +
+"\n- Special" +pSpecial
+);
+console.log(generatedPassword);
+return generatedPassword;
+}}
+
+//character functions
+function alphabetical(){
+    var alphabetArray = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", 
+                        "A", "S", "D", "F", "G", "H", "J", "K", "L", 
+                        "Z", "X", "C", "V", "B", "N", "M"];
+    var alphabetLength= alphabetArray.length;
+    var alphabetRandom= Math.floor(Math.random() *alphabetLength);
+    return alphabetArray[alphabetRandom];
+}
+function numeric(){
+    var numberArray= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var numberLength= numberArray.length;
+    var numberRandom= Math.floor(Math.random()*numberLength);
+    return numberArray[numberRandom];
+}
+function special(){
+    var specialArray=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-",
+                      "_", "=", "+"];
+    var specialLength= specialArray.length;
+    var specialRandom= Math.floor(Math.random()*specialLength);
+    return specialArray[specialRandom];
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
